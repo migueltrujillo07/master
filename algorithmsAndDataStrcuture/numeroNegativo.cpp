2f99bc1f6ecb3d79448d610d2610431c0ccb32c5
migueltrujillo <miguetrulo07@gmail.com>
2023-10-11 17:24:40 -0600

    Codigo de un numero negativo

---
+++ algorithmsAndDataStrcuture/numeroNegativo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  int numa = -5;
+  int numb = 2;
+  int numc = numa + numb;
+  cout << numc << endl;
+  cout << numc << endl;
+}
